d6455972c12eec0c0d023d487bb2f1b1a54cf073
Denys-Burduzha <106137997+Denys-Burduzha@users.noreply.github.com>
2022-10-24 11:10:23 +0100

    hw - 08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,34 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+import simpleLightbox  from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+
+console.log(galleryItems);
+
+const pictursContainer = document.querySelector('.gallery')
+const galleryMarkup = createPictureMarkup()
+
+// console.log(createPictureMarkup());
+pictursContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+function createPictureMarkup() {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+    })  
+        .join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
